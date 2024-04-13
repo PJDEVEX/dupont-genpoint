a672aad77676cbaa69f6be7efbae5a1a3202fb2b
PJDEVEX <piyankara.jayadewa@gmail.com>
2024-04-14 01:56:44 +0200

    feat: run experiments for the app and create init file stru

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'google-generativeai',
         'python-dotenv>=1.0.1',
         'streamlit>=1.33.0',
+        'python-dotenv',
+        'google-generativeai',
+        'google-cloud-aiplatform',
+        'google-api-python-client', 
+        'google-auth-httplib2 ',
+        'google-auth-oauthlib',
+        'google-cloud-storage',
     ]
 
 )
