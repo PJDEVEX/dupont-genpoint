cc97324266e14077da754335e4f18e53388dd9c1
PJDEVEX <piyankara.jayadewa@gmail.com>
2024-04-14 16:42:30 +0200

    init: Update project dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='practice-gemini',
+    name='dupont-genpoint',
     author='PJ',
     author_email='piyankra.jayadewa@gmail.com',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'python-dotenv>=1.0.1',
         'streamlit>=1.33.0',
         'python-dotenv',
-        'google-generativeai',
         'google-cloud-aiplatform',
         'google-api-python-client', 
         'google-auth-httplib2 ',
